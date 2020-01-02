0bc7d0b4917776a6e87c59218f494e59d7feabc9
Cioscos <claudiocicca1998@gmail.com>
2020-01-02 16:32:49 +0100

    Creazione nuova brench

---
+++ app/src/main/java/com/nullpointerexception/retrogames/Activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.nullpointerexception.retrogames.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nullpointerexception.retrogames.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
