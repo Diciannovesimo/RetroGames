ba7c47f97d9a19631e8af1ae31b9b638b80097d0
Cioscos <claudiocicca1998@gmail.com>
2020-01-02 17:04:32 +0100

    Resa login activity full screen

--- app/src/main/java/com/nullpointerexception/retrogames/Activities/LoginActivity.java
+++ app/src/main/java/com/nullpointerexception/retrogames/Activities/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.nullpointerexception.retrogames.Activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
 }
